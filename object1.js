37c5e99630c6141537bd542eb8a02115d187b108
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2025-01-21 19:24:50 +0200

    Update object1.js

--- object1.js
+++ object1.js
@@ -9,8 +9,6 @@
         renderer.toneMappingExposure = 1.5;
         document.body.appendChild(renderer.domElement);
 
-        renderer.domElement.classList.add('webgl-canvas');
-
 
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         scene.add(ambientLight);
